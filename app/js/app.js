b41fc61789ffa0185c844666a1d8eec94faac8c9
Marc <marc.bluemner@gmail.com>
2014-09-25 15:32:04 +0200

    done

--- app/js/app.js
+++ app/js/app.js
@@ -2,14 +2,14 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('angular_rest', [
+angular.module('myApp', [
   'ngRoute',
-  'angular_rest.filters',
-  'angular_rest.services',
-  'angular_rest.directives',
-  'angular_rest.controllers'
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives',
+  'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {templateUrl: 'partials/movie-list.html', controller: 'MovieListCtrl'});
-  $routeProvider.otherwise({redirectTo: '/'});
+  $routeProvider.when('/movie-list', {templateUrl: 'partials/movie-list.html', controller: 'MyCtrl1'});
+  $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
