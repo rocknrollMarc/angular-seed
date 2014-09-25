383f0d1497421899aaf78e4d7be54b2f86dfad1d
Marc <marc.bluemner@gmail.com>
2014-09-25 13:05:23 +0200

    rtm factory

--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,18 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('angular_rest.services', []).
+  value('version', '0.1')
+  
+  .factory('rtmFactory', function() {
+    var countries = [
+      {name: 'USA', code: 'us'},
+      {name: 'UK', code: 'uk'},
+      {name: 'France', code: 'fr'}
+    ];
+    return {
+      getCountries: function() {
+        return countries;
+      }
+    }
+  })
