24a93929833d64390d3b0b89adf6c77bf30816c7
Marc <marc.bluemner@gmail.com>
2014-09-25 13:47:36 +0200

    added jsonp for rotten api

--- app/js/services.js
+++ app/js/services.js
@@ -7,16 +7,22 @@
 // In this case it is a simple value service.
 angular.module('angular_rest.services', []).
   value('version', '0.1')
-  
-  .factory('rtmFactory', function() {
+.constant('API_KEY', 'jgqgdavk2923hwwk4ysqnjw2')
+
+.factory('rtmFactory', function() {
     var countries = [
-      {name: 'USA', code: 'us'},
-      {name: 'UK', code: 'uk'},
-      {name: 'France', code: 'fr'}
+      {name: 'USA',code: 'us'},
+      {name: 'UK',code: 'uk'},
+      {name: 'France',code: 'fr'}
     ];
     return {
       getCountries: function() {
         return countries;
+      },
+      getMovies: function(countryCode) {
+        var key = '';
+        return
+        $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?limit=10&country='+countryCode+'&callback=JSON_CALLBACK&apikey='+API_KEY); 
       }
-    }
-  })
+    };
+  });
