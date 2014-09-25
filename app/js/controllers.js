c20ad6aa93413460a21b21de97d29b08209acc14
Marc <marc.bluemner@gmail.com>
2014-09-25 14:10:26 +0200

    movie list partial

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,25 @@
 /* Controllers */
 
 angular.module('angular_rest.controllers', [])
-  .controller('MovieListCtrl', ['$scope', 'rtmFactory',
-             function($scope, rtmFactory) {
-               $scope.countries = rtmFactory.getCountries();
-}
-  ])
+.controller('MovieListCtrl', ['$scope', 'rtmFactory',
+  function($scope, rtmFactory) {
+    $scope.countries = rtmFactory.getCountries();
 
+    $scope.loadMovies = function(countryCode) {
+      rtmFactory.getMovies(countryCode).then(
+        function(response) {
+          
+          var movieArray = response.data.movies;
+          console.log(JSON.stringify(movieArray))
+          $scope.movies = movieArray;
+      },
+      function(response) {
+        // error message
+      }
+    )
+  };
+
+  $scope.loadMovies('us');
+
+  }
+])
--- app/js/services.js
+++ app/js/services.js
@@ -9,20 +9,22 @@ angular.module('angular_rest.services', []).
   value('version', '0.1')
 .constant('API_KEY', 'jgqgdavk2923hwwk4ysqnjw2')
 
-.factory('rtmFactory', function() {
+.factory('rtmFactory', ['$http','API_KEY',function($http,API_KEY) {
     var countries = [
-      {name: 'USA',code: 'us'},
-      {name: 'UK',code: 'uk'},
-      {name: 'France',code: 'fr'}
+    {name: 'USA',code: 'us'}, 
+    {name: 'UK',code: 'uk'}, 
+    {name: 'France',code: 'fr'}
     ];
     return {
-      getCountries: function() {
-        return countries;
-      },
-      getMovies: function(countryCode) {
-        var key = '';
-        return
-        $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?limit=10&country='+countryCode+'&callback=JSON_CALLBACK&apikey='+API_KEY); 
-      }
-    };
-  });
+        getCountries: function() {
+            return countries;
+        },
+            getMovies:function(countryCode){
+        
+        return $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?limit=10&country='+countryCode+'&callback=JSON_CALLBACK&apikey='+API_KEY)
+        
+   
+    }
+
+    }
+}])
